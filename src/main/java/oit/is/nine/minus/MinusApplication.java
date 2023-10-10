1db392b31a0b93bafe58d6561fff42474932de54
KosukeOkawa <e1b21020@oit.ac.jp>
2023-10-10 14:36:40 +0900

    初期化

---
+++ src/main/java/oit/is/nine/minus/MinusApplication.java
@@ -0,0 +1,13 @@
+package oit.is.nine.minus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinusApplication.class, args);
+	}
+
+}
